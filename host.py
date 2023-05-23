02457d345a7a45073f17a416535968bf681038c3
Joel Dice <joel.dice@fermyon.com>
2023-05-23 11:02:08 -0600

    add 5 second timeout to host.py
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- host.py
+++ host.py
@@ -1,15 +1,31 @@
 from sandbox import Sandbox
 from sandbox.types import Ok, Err
-from wasmtime import Store
+from wasmtime import Config, Engine, Store
 import json
 import sys
+from threading import Timer
+
+TIMEOUT_SECONDS=5
 
 args = sys.argv[1:]
 if len(args) == 0:
     print("usage: python3 host.py [<statement>...] <expression>", file=sys.stderr)
     exit(-1)
 
-store = Store()
+config = Config()
+config.epoch_interruption = True
+
+def on_timeout(engine):
+    print("timeout!")
+    engine.increment_epoch()
+
+engine = Engine(config)
+timer = Timer(TIMEOUT_SECONDS, on_timeout, args=(engine,))
+timer.start()
+
+store = Store(engine)
+store.set_epoch_deadline(1)
+
 sandbox = Sandbox(store)
 for arg in args[:-1]:
     result = sandbox.exec(store, arg)
@@ -25,3 +41,5 @@ if isinstance(result, Ok):
 else:
     result = result.value
     print(f"error: {result}")
+
+timer.cancel()
